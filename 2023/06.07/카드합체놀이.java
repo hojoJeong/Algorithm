e2a9eefa4092a3ffc19534ade26597dcd430c771
HoJoJeong <43957736+hojoJeong@users.noreply.github.com>
2023-06-07 23:57:55 +0900

    Create 카드합체놀이.java

---
+++ 2023/06.07/카드합체놀이.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 카드합체놀이_15903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        long ans = 0;
+
+        st = new StringTokenizer(br.readLine());
+        PriorityQueue<Long> prQueue = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            long num = Integer.parseInt(st.nextToken());    //숫자가 int 범위를 넘어서는지 잘 확인해야됨
+            prQueue.add(num);
+            ans += num;
+        }
+
+        for (int i = 0; i < m; i++) {
+            long x = prQueue.poll();
+            long y = prQueue.poll();
+            long sum = x + y;
+
+            prQueue.add(sum);
+            prQueue.add(sum);
+        }
+
+        if(m != 0){
+            ans = 0;
+            while (!prQueue.isEmpty()){
+                ans += prQueue.poll();
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
