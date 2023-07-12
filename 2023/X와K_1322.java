61335a59014967974e21c2de2217a79ff613388e
HoJoJeong <43957736+hojoJeong@users.noreply.github.com>
2023-07-12 23:21:35 +0900

    Create X와K_1322.java

---
+++ 2023/X와K_1322.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * X + Y == X | Y 이기 위해서는 X를 이진수로 변환했을 때 X의 n번째 자리가 1이면 Y의 n번째 자리는 0이어야함
+ * 나머지 자리는 K를 이진수로 변환한 후 0번째 자리수 부터 차례로 넣어주면 됨
+ * */
+
+public class X와K_1322 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long x = Long.parseLong(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+        String binaryX = Long.toBinaryString(x);
+        String binaryK = Long.toBinaryString(k);
+        StringBuilder result = new StringBuilder();
+        int idx = binaryK.length() - 1;
+
+        for (int i = binaryX.length() - 1; i >= 0; i--) {
+            char cur = binaryX.charAt(i);
+
+            if(cur == '0'){
+                if(idx == -1) continue;
+
+                result.insert(0, binaryK.charAt(idx));
+                idx--;
+            } else {
+                result.insert(0, 0);
+            }
+        }
+
+        while(idx >= 0){
+            result.insert(0, binaryK.charAt(idx));
+            idx--;
+        }
+
+        Long ans = Long.parseLong(result.toString(), 2);
+        System.out.println(ans);
+    }
+}
